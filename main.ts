b4dfab123ff534e6976f59a8abb20c42f981a245
August Matthew Quinlan <yoshicat336@gmail.com>
2021-03-14 09:41:46 -0700

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,20 @@
-basic.forever(function () {
-	
+enum RadioMessage {
+    message1 = 49434
+}
+input.onButtonPressed(Button.AB, function () {
+    RingbitCar.running_time(RingbitCar.Direction_run.forward, 10)
+    RingbitCar.steering_angle(RingbitCar.Direction_turn.right, 100)
+    RingbitCar.steering_angle(RingbitCar.Direction_turn.left, 100)
+    RingbitCar.running_distance(RingbitCar.Direction_run.forward, 100)
+    right = randint(-100, 100)
+    left = randint(-100, 100)
+    basic.pause(1000)
+    RingbitCar.freestyle(left, right)
 })
+input.onLogoEvent(TouchButtonEvent.Touched, function () {
+    RingbitCar.brake()
+})
+let left = 0
+let right = 0
+RingbitCar.init_wheel(AnalogPin.P1, AnalogPin.P2)
+basic.showIcon(IconNames.Heart)
